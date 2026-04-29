9becbbc37f861dace9d6f2414252f0761d599ba0
emorychien-debug <emorychien@gmail.com>
2026-04-29 10:04:15 -0500

    implemented nodes for tries implemented the add method implemented spellchecker constructor implemented getOneCharCompletions method implemented a variety of private helper methods to assist these methods

---
+++ src/main/java/edu/ttap/spellchecker/SpellChecker.java
@@ -0,0 +1,226 @@
+package edu.ttap.spellchecker;
+
+import java.io.IOException;
+import java.lang.invoke.WrongMethodTypeException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A spellchecker maintains an efficient representation of a dictionary for
+ * the purposes of checking spelling and provided suggested corrections.
+ */
+public class SpellChecker {
+    /** The number of letters in the alphabet. */
+    private static final int NUM_LETTERS = 26;
+
+    /** The path to the dictionary file. */
+    private static final String DICT_PATH = "files/words_alpha.txt";
+
+    /**
+     * @param filename the path to the dictionary file
+     * @return a SpellChecker over the words found in the given file.
+     */
+    public static SpellChecker fromFile(String filename) throws IOException {
+        return new SpellChecker(Files.readAllLines(Paths.get(filename)));
+    }
+
+    /** A Node of the SpellChecker structure. */
+    private class Node {
+        private ArrayList<Node> children;
+        private char letter;
+        private boolean isWord;
+
+        public Node(char ch, ArrayList<Node> wordEnds, boolean isWord) {
+            children = wordEnds;
+            letter = ch;
+            this.isWord = isWord;
+        }
+        public Node(char ch, boolean isWord) {
+            this(ch, new ArrayList<Node>(), isWord);
+        }
+        public boolean addNode(Node n) {
+            for(Node cur: children) {
+                if(n.getLetter() == cur.getLetter()) {
+                    return false;
+                }
+            }
+            children.add(n);
+            return true;
+        }
+        public boolean isWord(){
+            return isWord;
+        }
+        public void setWord() {
+            isWord = true;
+        }
+        public char getLetter() {
+            return letter;
+        }
+        public Node getChild(char ch) {
+            for(int i = 0; i < children.size(); i++) {
+                if(children.get(i).getLetter() == ch) {
+                    return children.get(i);
+                }
+            }
+            return null;
+        }
+        public boolean hasChild(char ch) {
+            for(Node n: children) {
+                if(n.getLetter() == ch) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /** The root of the SpellChecker */
+    private Node root;
+
+    /**
+     * adds a word to the trie
+     * @param word the word to be added
+     */
+    private void addWord(String word) {
+        char[] wordArray = word.toCharArray();
+        Node curNode = root;
+        for(int i = 0; i < wordArray.length; i++) {
+            Node newestNode = curNode.getChild(wordArray[i]);
+            if(newestNode != null) {
+                curNode = newestNode;
+            } else {
+                newestNode = new Node(wordArray[i], new ArrayList<Node>(), false);
+                curNode.addNode(newestNode);
+                curNode = newestNode;
+            }
+            if(i == wordArray.length - 1) {
+                curNode.setWord();
+            }
+        }
+    }
+    
+    /**
+     * Constructs a SpellChecker over the given dictionary.
+     * @param dict the list of words to include in the dictionary
+     */
+    public SpellChecker(List<String> dict) {
+        root = new Node('\0', new ArrayList<>(), false);
+        for(String word : dict) {
+            addWord(word);
+        }
+    }
+
+    /**
+     * Adds the given word to the trie
+     * @param word the word to add
+     */
+    public void add(String word) {
+        addWord(word);
+    }
+
+    /**
+     * Checks if the given word is in the dictionary.
+     * @param word the word to check
+     * @return true if the word is in the dictionary, false otherwise
+     */
+    public boolean isWord(String word) {
+        char[] wordArray = word.toCharArray();
+        Node curNode = root;
+        for(char ch: wordArray) {
+            curNode = curNode.getChild(ch);
+            if(curNode == null) {
+                return false;
+            }
+        }
+        return curNode.isWord();
+    }
+
+    /**
+     * Returns a list of all words in the dictionary that can be formed by
+     * adding a single character to the end of the given word.
+     * @param word the word to complete
+     * @return a list of all possible completions
+     */
+    public List<String> getOneCharCompletions(String word) {
+        char[] wordArray = word.toCharArray();
+        Node curNode = root;
+        for(int i = 0; i < wordArray.length; i++) {
+            curNode = curNode.getChild(wordArray[i]);
+            if(curNode == null) {
+                return new ArrayList<String>();
+            }
+        }
+        List<String> otherEndings = new ArrayList<>();
+        for(Node n : curNode.children) {
+            otherEndings.add(word + n.getLetter());
+        }
+        return otherEndings;
+    }
+
+
+    /**
+     * Returns a list of all words in the dictionary that can be formed by changing
+     * a single character in the given word.
+     * @param word the word to correct
+     * @return a list of all possible corrections
+     */
+    public List<String> getOneCharEndCorrections(String word) {
+        // TODO: implement me!
+        return null;
+    }
+
+    /**
+    * Returns a list of all words in the dictionary that can be formed by adding,
+    * removing, or changing a single character in the given word.
+    * @param word the word to correct
+    * @return a list of all possible corrections
+    */
+    public List<String> getOneCharCorrections(String word) {
+        // TODO: implement me!
+        return null;
+    }
+
+    /**
+     * The main entry point for the program.
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Usage: java SpellChecker <command> <word>");
+            System.exit(1);
+        } else {
+            String command = args[0];
+            String word = args[1];
+            SpellChecker checker = SpellChecker.fromFile(DICT_PATH);
+            switch (command) {
+                case "check": {
+                    System.out.println(checker.isWord(word) ? "correct" : "incorrect");
+                    System.exit(0);
+                }
+
+                case "complete": {
+                    List<String> completions = checker.getOneCharCompletions(word);
+                    for (String completion : completions) {
+                        System.out.println(completion);
+                    }
+                    System.exit(0);
+                }
+
+                case "correct": {
+                    List<String> corrections = checker.getOneCharEndCorrections(word);
+                    for (String correction : corrections) {
+                        System.out.println(correction);
+                    }
+                    System.exit(0);
+                }
+
+                default: {
+                    System.err.println("Unknown command: " + command);
+                    System.exit(1);
+                }
+            }
+        }
+    }
+}
